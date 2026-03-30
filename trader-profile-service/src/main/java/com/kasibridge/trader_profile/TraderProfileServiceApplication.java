64195af3cca1b73c6bbc11e051ca9edc57ab3350
Mfundo Moloya <moloyamfundo@gmail.com>
2026-03-30 22:42:59 +0200

    feat: scaffold trader-profile-service

---
+++ trader-profile-service/src/main/java/com/kasibridge/trader_profile/TraderProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kasibridge.trader_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderProfileServiceApplication.class, args);
+	}
+
+}
---
+++ trader-profile-service/src/test/java/com/kasibridge/trader_profile/TraderProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasibridge.trader_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
