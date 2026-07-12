f2e2324e035705a66a36012101a70f758b99999c
Mfundo Moloya <moloyamfundo@gmail.com>
2026-07-12 04:27:00 +0200

    feat(procurement): add procurement service module

---
+++ procurement-service/src/main/java/com/kasibridge/procurementservice/ProcurementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kasibridge.procurementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcurementServiceApplication.class, args);
+    }
+
+}
---
+++ procurement-service/src/test/java/com/kasibridge/procurementservice/ProcurementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasibridge.procurementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
